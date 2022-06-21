04abf54459c6c91a07ee89f03861643741d066dc
Pierre Leroux <panacpp@gmail.com>
2022-06-21 19:16:54 +0200

    up

--- telemetry/curl.sh
+++ telemetry/curl.sh
@@ -4,3 +4,4 @@ curl -H "Authorization: EDRLAB " -v http://localhost:8090
 curl -H "Authorization: EDRLAB rocks" -v http://localhost:8090
 
 
+curl -H "Authorization: EDRLAB 4821f05cf54aaae81aaab53df3d8b2f07c049ba1" -d "{"timestamp":"2022-06-21T17:14:45.929Z"}" -v http://localhost:8090
--- telemetry/lua/script.lua
+++ telemetry/lua/script.lua
@@ -1,6 +1,7 @@
+local hmac = require "resty.hmac"
 
 local authorizationHeader = ngx.req.get_headers()["Authorization"];
-ngx.log(ngx.DEBUG, string.format("Authorization: %s", authorizationHeader));
+print(string.format("Authorization: %s", authorizationHeader));
 
 if authorizationHeader == nil or authorizationHeader == '' then
   ngx.status = ngx.HTTP_BAD_REQUEST;
@@ -9,10 +10,48 @@ if authorizationHeader == nil or authorizationHeader == '' then
 end
 
 local authorizationKey = string.match(authorizationHeader, "EDRLAB (.*)")
-ngx.log(ngx.DEBUG, string.format("Authorization key: %s", authorizationKey));
+-- print(string.format("Authorization key: %s", authorizationKey));
 
 if authorizationKey == nil or authorizationKey == '' then
   ngx.status = ngx.HTTP_BAD_REQUEST;
   ngx.say("ERROR: not a valid authorization key");
   return ngx.exit(ngx.OK);
 end
+
+local hmac_sha1 = hmac:new("hello world", hmac.ALGOS.SHA1);
+
+if not hmac_sha1 then
+	ngx.log(ngx.ERR, "failed to create the hmac_sha1 object");
+  ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR;
+  return ngx.exit(ngx.OK);
+end
+
+ngx.req.read_body();
+local data = ngx.req.get_body_data(); -- return lua string instead table
+if data == nil or data == '' then
+  ngx.status = ngx.HTTP_BAD_REQUEST;
+  ngx.say("ERROR: No body available");
+  return ngx.exit(ngx.OK);
+end
+
+local ok = hmac_sha1:update(data)
+if not ok then
+	ngx.log(ngx.ERR, "failed to add body data to hmac");
+  ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR;
+  return ngx.exit(ngx.OK);
+end
+
+local hmacKey = hmac_sha1:final(nil, true);
+
+if not hmac_sha1:reset() then
+	ngx.log(ngx.ERR, "failed to reset hmac");
+end
+
+if hmacKey ~= authorizationKey then
+	ngx.log(ngx.ERR, string.format("hmac key (%s) doesn't match with authorization key (%s)", hmacKey, authorizationKey));
+  ngx.status = ngx.HTTP_UNAUTHORIZED;
+  ngx.say("ERROR: Bad authorization");
+  return ngx.exit(ngx.OK);
+end
+
+print("Authorization OK");
